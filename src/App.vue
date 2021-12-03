8cf249f4c0be294e07d09acce606f3074ddb9eaf
saacalves <saacalvesi3solucoes@gmail.com>
2021-12-03 10:16:23 -0300

    Alterações Router

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <v-app>
     <v-app-bar app color="black" dark>
-      <div class="d-flex align-center">
-        <v-btn 
-        color="white" 
-        text 
-        @click="goToHome"
-        >
-        Cadastro
-        </v-btn>
-      </div>
-      <div class="d-flex align-center">
-        <v-btn 
-        color="white" 
-        text 
-        @click="goToEstante"
-        >
-        Estante
-        </v-btn>
-      </div>
+    <div id="nav">
+      <router-link to="/home">CADASTRO</router-link> |
+      <router-link to="/estante">ESTANTE</router-link> 
+    </div>
+    <router-view/>
       <v-spacer></v-spacer>
         <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>   
         <span class="mr-2">Latest Release</span>
